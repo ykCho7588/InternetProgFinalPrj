d00a98e4316ffdd119be668b9616830f685e8dd3
Yoon-kyeong Cho <dbsrud2788@duksung.ac.kr>
2021-12-19 14:06:14 +0900

    아이템 수정기능 추가하기

--- shop/urls.py
+++ shop/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     # path('<int:pk>/', views.single_item_page),
     # path('', views.index),
+    path('update_item/<int:pk>/', views.ItemUpdate.as_view()),
     path('create_item/', views.ItemCreate.as_view()),
     path('category/<str:slug>', views.category_page),
     path('<int:pk>/', views.ItemDetail.as_view()),
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,6 @@
+from django.core.exceptions import PermissionDenied
 from django.shortcuts import render, redirect
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Item, Category
 # Create your views here.
@@ -11,7 +12,6 @@ class ItemCreate(LoginRequiredMixin, CreateView):
     def test_func(self):
         return self.request.user.is_superuser or self.request.user.is_staff
 
-
     def form_valid(self, form):
         current_user = self.request.user
         if current_user.is_authenticated and (current_user.is_staff or current_user.is_superuser):
@@ -19,6 +19,18 @@ class ItemCreate(LoginRequiredMixin, CreateView):
             return super(ItemCreate, self).form_valid(form)
         else:
             return redirect('/shop/')
+
+class ItemUpdate(LoginRequiredMixin, UpdateView):
+    model = Item
+    fields = ['item_name', 'item_info', 'item_price', 'head_image', 'corp_name', 'category', 'item_size', 'item_color']
+
+    template_name = 'shop/item_update_form.html'
+
+    def dispatch(self, request, *args, **kwargs): #get or post 방식을 구별해주는 함수
+        if request.user.is_authenticated and request.user == self.get_object().author : #요청 사용자가 로그인하였고 해당 포스트의 작성자이면
+            return super(ItemUpdate, self).dispatch(request, *args, **kwargs)
+        else :
+            raise PermissionDenied
 
 class ItemList(ListView):
     model = Item
