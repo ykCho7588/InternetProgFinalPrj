7d4ace754350120a1ad8ece189006f118ffacc0c
Yoon-kyeong Cho <dbsrud2788@duksung.ac.kr>
2021-12-19 19:22:21 +0900

    대문 페이지 수정하기

--- single_pages/views.py
+++ single_pages/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from shop.models import Item
+
+
 def landing(request):
-    return render(request, 'single_pages/landing.html')
+    popular_items = Item.objects.order_by('pk')[:3]
+    return render(request, 'single_pages/landing.html',
+                  {'popular_items' : popular_items})
 
 def about_us(request):
     return render(request, 'single_pages/about_us.html')
